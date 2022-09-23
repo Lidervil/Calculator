d5bfb6389ff976dedb52df526e7ec2e4ac4c849b
ImomushiRan <george.villarroel@outlook.es>
2022-09-22 23:26:37 -0300

    Add full Js functions, need eventlisteners

--- index.js
+++ index.js
@@ -18,21 +18,78 @@ const plus = document.querySelector('#plus');
 const equals = document.querySelector('#equals');
 console.log(main)
 
-const arr = []
+var arr = []
 var actNumber= '';
 var dispTop;
 var dispBot;
 
 function add(num) {
     actNumber += num;
+    let arrDisplay = '';
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] !== undefined) {
+            arrDisplay += arr[i];
+        }
+    }
+    let display = arrDisplay+actNumber;
     main.innerHTML= actNumber;
+    sub.innerHTML = display;
 }
 
 function operation(type) {
-    arr.push(parseFloat(actNumber))
+    arr.push(parseFloat(actNumber));
     actNumber= '';
-                                            
+    main.innerHTML= '0';
+    let length = arr.length;
+    let operNumber;
+    if (length < 3) {
+        arr.push(type);
+    } else if (arr[length-2] == '/') {
+        operNumber = arr[length-3] / arr[length-1];
+        arr = [operNumber,type];
+        main.innerHTML= operNumber;
+    } else if (arr[length-2] == '*') {
+        operNumber = arr[length-3] * arr[length-1];
+        arr = [operNumber,type];
+        main.innerHTML= operNumber;
+    } else if (arr[length-2] == '-') {
+        operNumber = arr[length-3] - arr[length-1];
+        arr = [operNumber,type];
+        main.innerHTML= operNumber;
+    } else if (arr[length-2] == '+') {
+        operNumber = arr[length-3] + arr[length-1];
+        arr = [operNumber,type];
+        main.innerHTML= operNumber;
+    }
 }
+
+function equals() {
+    if (actNumber != '') {
+        arr.push(parseFloat(actNumber));
+    } else {
+    }
+    if (length < 3) {
+        if (arr[0] != undefined) {
+            main.innerHTML=arr[0];
+        } else {
+            main.innerHTML= '0';
+        }
+    } else if (arr[length-2] == '/') {
+        operNumber = arr[length-3] / arr[length-1];
+        main.innerHTML= operNumber;
+    } else if (arr[length-2] == '*') {
+        operNumber = arr[length-3] * arr[length-1];
+        main.innerHTML= operNumber;
+    } else if (arr[length-2] == '-') {
+        operNumber = arr[length-3] - arr[length-1];
+        main.innerHTML= operNumber;
+    } else if (arr[length-2] == '+') {
+        operNumber = arr[length-3] + arr[length-1];
+        main.innerHTML= operNumber;
+    }
+    arr = [];
+}
+
 /*
 number 1+number 2+ number3 +number4
 
